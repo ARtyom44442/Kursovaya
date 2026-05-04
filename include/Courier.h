ef1b44b98e83c7c680a8db78a51e433250591285
Daniil Tarasenko <tarasenko_dd_25@student.grsu.by>
2026-05-04 14:14:21 +0300

    Create Courier.h
    добавил класс Courier

---
+++ include/Courier.h
@@ -0,0 +1,17 @@
+#include "C:\k\Kursovaya\include\Transport.h"
+
+class Courier:
+public Transport{
+private:
+    float max_radius;
+
+public:
+     Courier(std::string name, float s, float max_w, float max_v, coords pos, float radius);
+
+     bool canHandle(Order& order);
+
+     float getmax_radius(){return max_radius;}
+     void setmax_radius(float radius){max_radius = radius;}
+     void PrintStats();
+
+};
