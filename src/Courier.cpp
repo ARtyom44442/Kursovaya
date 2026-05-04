cbedb0ec210c5c3028f3ea37743667d76b5b1861
Daniil Tarasenko <tarasenko_dd_25@student.grsu.by>
2026-05-04 14:16:30 +0300

    Create Courier.cpp
    добавил функцию, которая проверяет может ли довести её курьер и может находится ли точка в которую надо довести в его радиусе работы

---
+++ src/Courier.cpp
@@ -0,0 +1,17 @@
+#include "Courier.h"
+#include <iostream>
+#include <cmath>
+
+Courier::Courier(std::string name, float s, float max_w, float max_v, coords pos, float radius): Transport(name, s, max_w, max_v, pos), max_radius(radius) {}
+
+bool Courier::canHandle(Order& order) {
+    if (Transport::canHandle(order) == false) {
+        return false;
+    }
+    coords dest = order.getDestination();
+    coords currentPos = getCurrentPos();
+    float dist = std::sqrt(std::pow(dest.x - currentPos.x, 2) + std::pow(dest.y - currentPos.y, 2));
+    return dist <= max_radius;
+}
+
+void Courier::PrintStats() {}
