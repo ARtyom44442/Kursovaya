0a5903cbd8f1029b9e521b0288117acd7f7800ed
Daniil Tarasenko <tarasenko_dd_25@student.grsu.by>
2026-05-06 14:08:45 +0300

    Create Truck.h
    добавил класс Truck

---
+++ include/Truck.h
@@ -0,0 +1,19 @@
+#include "C:\k\Kursovaya\include\Transport.h"
+#include <cstdlib>
+#include <ctime>
+
+class Truck : public Transport {
+private:
+    float traffic_factor_min = 1.0; 
+    float traffic_factor_max = 1.5;
+public:
+    Truck(const std::string& name, float s, float max_w, float max_v, coords pos);
+    float calculateTime(Order& order);
+    float gettraffic_factor_min() { return traffic_factor_min; }
+    float gettraffic_factor_max() { return traffic_factor_max; }
+    void settrafficfactorrange(float min, float max);
+  
+    void PrintStats();
+private:
+    float getRandomTrafficFactor();
+};
