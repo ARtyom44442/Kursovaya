9230b7c72e569a0ba12b6d27641f1d03a4e1ab69
Daniil Tarasenko <tarasenko_dd_25@student.grsu.by>
2026-05-10 08:39:06 +0300

    Add files via upload
    Исправлена ошибка с вводом отрицательных чисел, теперь сразу выбивает ошибку

---
+++ Kursovaya/include/order.h
@@ -0,0 +1,29 @@
+#pragma once
+
+struct coords {
+    int x, y;
+};
+
+class Order {
+private:
+    int ID;
+    float weight;
+    float vol;
+    coords destination;
+    int max_time;
+public:
+    Order(int id, float w, float v, coords d, int mt);
+
+    int getID() { return ID; }
+    float getWeight() { return weight; }
+    float getVol() { return vol; }
+    int getMaxTime() { return max_time; }
+    coords getDestination() { return destination; }
+
+    void setDestination(coords d);
+    void setWeight(float w);
+    void setVol(float v);
+    void setMaxTime(int mt);
+
+    void PrintStats();
+};
